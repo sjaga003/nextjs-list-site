a5e0afab4a7146bd0c7f392d0e24abe39d44ee1c
Suhas Jagannath <sjaga003@ucr.edu>
2021-05-08 15:46:02 -0700

    Images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,34 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
-        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
-        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
-        deleniti.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
-        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
-        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
-        deleniti.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
-        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
-        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
-        deleniti.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          cum consectetur alias nemo animi doloremque ex dolores dignissimos
+          adipisci enim illum nostrum, veniam modi distinctio earum ipsam cumque
+          iure deleniti.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          cum consectetur alias nemo animi doloremque ex dolores dignissimos
+          adipisci enim illum nostrum, veniam modi distinctio earum ipsam cumque
+          iure deleniti.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          cum consectetur alias nemo animi doloremque ex dolores dignissimos
+          adipisci enim illum nostrum, veniam modi distinctio earum ipsam cumque
+          iure deleniti.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,29 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        itaque, repudiandae quos sed eaque doloremque dolor animi mollitia odit
-        qui alias nostrum voluptatem! Maxime rerum ab nobis corrupti, harum
-        reiciendis?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-        itaque, repudiandae quos sed eaque doloremque dolor animi mollitia odit
-        qui alias nostrum voluptatem! Maxime rerum ab nobis corrupti, harum
-        reiciendis?
-      </p>
-      <Link href="/ninjas" className="btn">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          itaque, repudiandae quos sed eaque doloremque dolor animi mollitia
+          odit qui alias nostrum voluptatem! Maxime rerum ab nobis corrupti,
+          harum reiciendis?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          itaque, repudiandae quos sed eaque doloremque dolor animi mollitia
+          odit qui alias nostrum voluptatem! Maxime rerum ab nobis corrupti,
+          harum reiciendis?
+        </p>
+        <Link href="/ninjas" className="btn">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
