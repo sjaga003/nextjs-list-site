447ef5e2b8fd71b3e4c6ca7f7d2c29c6400c27b2
Suhas Jagannath <sjaga003@ucr.edu>
2021-05-08 14:50:29 -0700

    Create components to be used in pages

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
+        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
+        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
+        deleniti.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
+        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
+        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
+        deleniti.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cum
+        consectetur alias nemo animi doloremque ex dolores dignissimos adipisci
+        enim illum nostrum, veniam modi distinctio earum ipsam cumque iure
+        deleniti.
+      </p>
+    </div>
+  );
+};
+
+export default About;
